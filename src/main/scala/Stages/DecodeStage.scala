ce943944c0361d1b8f08fa6e07bf7399790d2bef
BertramAP <bedeampo@gmail.com>
2026-01-06 14:38:03 +0100

    I have implemented the fileregister to decodestage

--- src/main/scala/Stages/DecodeStage.scala
+++ src/main/scala/Stages/DecodeStage.scala
@@ -2,6 +2,11 @@ import chisel3._
 import chisel3.util._
 
 class DecodeStage extends Module {
+
+  // Helper function for sign-extending I-type immediates
+  def signExtendIType(instr: UInt): UInt = {
+    Cat(Fill(20, instr(31)), instr(31, 20))
+  }
   val io = IO(new Bundle {
     // Inputs from the Fetch stage
     val instr = Input(0.U(32.W))
@@ -15,48 +20,64 @@ class DecodeStage extends Module {
     val funct7 = Output(0.U(1.W))
   })
   val aluOp = RegInit(0.U(4.W))
+  val registerFile = Module(new RegisterFile())
   val src1 = RegInit(0.U(32.W))
   val src2 = RegInit(0.U(32.W))
   val dest = RegInit(0.U(32.W))
   val funct3 = RegInit(0.U(3.W))
-  val opcode = RegInit(io.instr(6, 0))
-  val rd = RegInit(io.instr(11, 7))
+  val opcode = Wire(UInt(7.W))
+  opcode := io.instr(6, 0)
+  val rd = Wire(UInt(5.W))
+  rd := io.instr(11, 7)
   val funct7 = RegInit(0.U(1.W))
+
   switch(opcode) {
-    is(0x13.U) { // I-Type
-      val imm = (io.instr(31, 20)).asSINT.pad(20).asUInt
+    is(19.U) { // I-Type
+      val imm = signExtendIType(io.instr)
       src1 := (io.instr(19, 15))
       dest := rd
       funct3 := io.instr(14, 12)
       when(funct3 === 0x1.U || funct3 === 0x5.U) {
-        io.src2 := imm(4,0)
+        src2 := imm(4,0)
         // TODO: handle funct7 for slli and srli
         funct7 := io.instr(30)
       }.otherwise {
-        src2 := imm
+        src2 := imm(11, 0)
       }
-    is(0x33.U) { // R-type
+    } 
+    is(51.U) { // R-type
       dest := rd
       src1 := io.instr(19, 15)
-      src2 := io.instr(24, 20)
+      src2 := (io.instr(31, 20)).asSInt().asUInt()
       funct3 := io.instr(14, 12)
       funct7 := io.instr(30)
-    } is(0x03.U) { // Load
+    } 
+    is(3.U) { // Load type
+      val imm = signExtendIType(io.instr)
       src1 := (io.instr(19, 15))
-      src2 := (io.instr(31, 20)).asSINT.pad(20).asUInt
+      src2 := imm(11,0)
       funct3 := io.instr(14, 12)
       dest := rd
-    } is(0x23.U) { // Store
-      
-    }
-
-  // Implement the decode stage logic here
+    } 
+    is(35.U) { // Store type
+      src1 := (io.instr(19, 15))
+    } 
   }
+  registerFile.io.readRegister1 := src1
+  registerFile.io.readRegister2 := src2
+  registerFile.io.writeRegister := 0.U
+  registerFile.io.writeData := 0.U
+  registerFile.io.regWrite := false.B
   io.aluOp := aluOp
-  io.src1 := src1
-  io.src2 := src2
+  io.src1 := registerFile.io.readData1
+  io.src2 := registerFile.io.readData2
   io.dest := dest
   io.funct3 := funct3
   io.funct7 := funct7
 
+}
+
+// You need this "Object" to run the generator!
+object DecodeStage extends App {
+  emitVerilog(new DecodeStage(), Array("--target-dir", "generated"))
 }
