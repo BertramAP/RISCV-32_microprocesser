6df5a61e09eddaa4ed7f889e44dfc3592c8e4b15
LuskyMJ <mosquito1306@gmail.com>
2026-01-08 14:52:36 +0100

    Reorganized Pipelines

--- src/main/scala/Stages/PipelineBundles.scala
+++ src/main/scala/Stages/PipelineBundles.scala
@@ -13,7 +13,6 @@ class FetchBranchIO extends Bundle {
 }
 
 class DecodeExecuteIO extends Bundle {
-  val aluOp  = UInt(4.W)
   val src1   = UInt(32.W)
   val src2   = UInt(32.W)
   val imm    = UInt(32.W)
@@ -22,15 +21,23 @@ class DecodeExecuteIO extends Bundle {
   val funct7 = UInt(1.W)
   val pc = UInt(32.W)
   val isPC = Bool()
-  val RegWrite = Bool()
-  val ALUSrc = Bool()
-  val PCSrc = Bool()
   val isJump = Bool()
+  val isJumpr = Bool()
   val isBranch = Bool()
-  val MemRead = Bool()
+
+  // Control signals for execute stage
+  val ALUSrc = Bool()
+  val aluOp  = UInt(4.W)
+
+  // Control signals for memory stage
   val MemWrite = Bool()
+  val MemRead = Bool()
+
+  // Control signals for writeback stage
+  val RegWrite = Bool()
   val MemToReg = Bool()
 }
+
 class DecodeOutputsIO extends Bundle {
   val aluOp  = UInt(4.W)
   val src1   = UInt(5.W)
@@ -43,8 +50,8 @@ class DecodeOutputsIO extends Bundle {
   val isPC = Bool()
   val RegWrite = Bool()
   val ALUSrc = Bool()
-  val PCSrc = Bool()
   val isJump = Bool()
+  val isJumpr = Bool()
   val isBranch = Bool()
   val MemRead = Bool()
   val MemWrite = Bool()
@@ -75,19 +82,18 @@ class MemWbIO extends Bundle {
     // Control signal for writeback stage
     val wbRegWrite = Bool()
     val wbMemToReg = Bool()
-
 }
 
 
 class DecodeInputIO extends Bundle {
   val instr = UInt(32.W)
   val pc    = UInt(32.W)
-  
 }
 class ControllerExecuteIO extends Bundle {
   val RegWrite = Bool()
   val ALUSrc = Bool()
   val isJump = Bool()
+  val isJumpr = Bool()
   val isBranch = Bool()
   val MemRead = Bool()
   val MemWrite = Bool()
