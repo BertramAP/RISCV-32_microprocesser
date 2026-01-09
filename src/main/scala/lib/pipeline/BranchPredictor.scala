ff96eac5bd8f43de0a95a071976bb30a47c5561b
BertramAP <bedeampo@gmail.com>
2026-01-09 14:23:30 +0100

    Quick fix to branch predictor, it wasnt dependen on if the loaded instruction was b-type

--- src/main/scala/lib/pipeline/BranchPredictor.scala
+++ src/main/scala/lib/pipeline/BranchPredictor.scala
@@ -4,38 +4,42 @@ import chisel3.util._
 
 class BranchPredictor extends Module {
   val io = IO(new Bundle {
-    val isBranch = Input(Bool())
-    val branchTaken = Input(Bool())
+    val isBranch = Input(Bool()) // Indicates if the current instruction is a branch
+    val branchTaken = Input(Bool()) // Actual outcome of the branch from exe stage
 
-    val predictTaken = Output(Bool())
+    val predictTaken = Output(Bool()) // Prediction output
   })
   val counter = RegInit(0.U(2.W))
 
   switch(counter) {
     is(0.U) {
-      when(io.branchTaken) {
+      when(io.isBranch && io.branchTaken) {  
         counter := 1.U
       }
       io.predictTaken := false.B
     }
     is(1.U) {
-      when(io.branchTaken) {
-        counter := 2.U
-      } .otherwise {
-        counter := 0.U
+      when(io.isBranch) { 
+        when(io.branchTaken) {
+          counter := 2.U
+        } .otherwise {
+          counter := 0.U
+        }
       }
     io.predictTaken := false.B
     }
     is(2.U) {
-      when(io.branchTaken) {
-        counter := 3.U
-      } .otherwise {
-        counter := 1.U
+      when(io.isBranch) {
+        when(io.branchTaken) {
+          counter := 3.U
+        } .otherwise {
+          counter := 1.U
+        }
       }
       io.predictTaken := true.B
     }
     is(3.U) {
-      when(!io.branchTaken) {
+      when(io.isBranch && !io.branchTaken) {
         counter := 2.U
       }
       io.predictTaken := true.B
