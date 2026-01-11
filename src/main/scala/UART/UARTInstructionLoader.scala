a1a2170b22783945ba86562740f4cc710efaa4bb
BertramAP <bedeampo@gmail.com>
2026-01-11 14:30:10 +0100

    constant valuas

--- src/main/scala/UART/UARTInstructionLoader.scala
+++ src/main/scala/UART/UARTInstructionLoader.scala
@@ -2,3 +2,15 @@
 import chisel3._
 import chisel3.util._
 
+class UARTInstructionLoader() extends Module {
+    val io = IO(new Bundle {
+    val uartRx = Input(Bool())
+    val loadDone = Output(Bool())
+
+  })
+  val baudRate = 115200
+  val clockFreq = 100000000 // 100 MHz
+  val divisor = clockFreq / baudRate
+
+  
+}
