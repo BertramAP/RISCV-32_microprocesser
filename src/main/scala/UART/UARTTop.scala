a3fc264eb9ebab9178e7f4e96e5a217b827ddd78
BertramAP <bedeampo@gmail.com>
2026-01-12 12:01:41 +0100

    Fixed some incorrectly named names for ports

--- src/main/scala/UART/UARTTop.scala
+++ src/main/scala/UART/UARTTop.scala
@@ -6,19 +6,19 @@ import chisel3.util._
 
 class UARTTop() extends Module {
   val io = IO(new Bundle {
-    val uartRx = Input(Bool()) // UART receive line
-    val io_led = Output(Bool())
+    val rx = Input(Bool()) // UART receive line
+    val led = Output(Bool())
   })
 
   val instructionLoader = Module(new UARTInstructionLoader())
-  instructionLoader.io.uartRx := io.uartRx
+  instructionLoader.io.uartRx := io.rx
 
   // Simple LED indicator: turn on when loading is done
   val loadDoneReg = RegInit(false.B)
   when(instructionLoader.io.loadDone) {
     loadDoneReg := true.B
   }
-  io.io_led := loadDoneReg
+  io.led := loadDoneReg
 }
 object UARTTop extends App {
     emitVerilog(new UARTTop(), Array("--target-dir", "generated"))
