761a1e501dfb000b081ca35b6b6e0eddc1eb42c0
BertramAP <bedeampo@gmail.com>
2026-01-12 11:33:53 +0100

    Fixed initalliase issue in UARTInstructionLoader

--- src/main/scala/UART/UARTInstructionLoader.scala
+++ src/main/scala/UART/UARTInstructionLoader.scala
@@ -20,9 +20,10 @@ class UARTInstructionLoader() extends Module {
 
   val sIdle :: sStart :: sData :: sStop :: sDone :: Nil = Enum(5)
   val state = RegInit(sIdle)
-  
+  val done = WireDefault(false.B)
   switch(state) {
     is(sIdle) {
+      done := false.B
       when(!io.uartRx) {
         state := sStart
         counter := 0.U
@@ -68,11 +69,11 @@ class UARTInstructionLoader() extends Module {
     }
    }
    is(sDone) {
-    io.loadDone := true.B
+    done := true.B
     io.transferData := dataReg
     state := sIdle
    }
   }
   io.transferData := 0.U
-  io.loadDone := false.B
+  io.loadDone := done
 }
