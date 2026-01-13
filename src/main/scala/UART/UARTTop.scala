2bf19c1e7182a4bdb450f5eaaf6da18c14bd5a37
BertramAP <bedeampo@gmail.com>
2026-01-13 14:55:02 +0100

    The UARTTop can now write to both instruction and data memory

--- src/main/scala/UART/UARTTop.scala
+++ src/main/scala/UART/UARTTop.scala
@@ -9,20 +9,75 @@ class UARTTop() extends Module {
     val rx = Input(Bool()) // UART receive line
     val led = Output(UInt(8.W)) // LED indicator
   })
-  val instructionMem = SyncReadMem(1024, UInt(32.W)) // 256 bytes instruction memory
+  // memory
+  val instructionMem = SyncReadMem(1024, UInt(8.W)) // Byte memory
+  val dataMem = SyncReadMem(1024, UInt(8.W)) // Byte memory
+  val memUsed = RegInit(0.U(1.W)) // 1 for data memory, 0 for instruction memory
+
+  // boolean for loading header:
+  val length = RegInit(0.U(24.W))
+  val loadCounter = RegInit(0.U(2.W)) // length is 3 bytes, loaded in one by one
   val instructionLoader = Module(new UARTInstructionLoader())
   val pc = RegInit(0.U(32.W))
   instructionLoader.io.uartRx := io.rx
-  val loadDoneReg = RegInit(0.U(8.W))
 
+
+  
+  val loadDoneReg = RegInit(0.U(8.W))
+  /*
   // Simple LED indicator: turn on when loading is done
   io.led := loadDoneReg
   when(instructionLoader.io.loadDone) { // Write
+    // TODO: concatenate bytes to form instructions, and reset instruction memory before loading new program
     instructionMem.write(pc, instructionLoader.io.transferData)
     loadDoneReg := instructionMem.read(pc)
     pc := pc + 1.U
     //loadDoneReg := instructionLoader.io.transferData
+  } */ // From earlier tests
+
+  val sIdle :: loadLength :: sLoadData :: Nil = Enum(3)
+  val state = RegInit(sIdle)
+  switch(state) {
+    is(sIdle) { // We assume first loaded byte is an header
+      when(instructionLoader.io.loadDone) {
+        memUsed := instructionLoader.io.transferData(0) // LSB indicates memory type
+        pc := 0.U // Reset pc for loading
+        state := loadLength
+      }
+    }
+    is(loadLength) {
+      when(instructionLoader.io.loadDone) {
+        length := Cat(instructionLoader.io.transferData(7,0), length(23, 8)) // Little endian
+        loadCounter := loadCounter + 1.U
+        when(loadCounter === 2.U) { // Should maybe be 3.U?
+          state := sLoadData
+          loadCounter := 0.U
+        }
+      }
+    }
+    is(sLoadData) {
+      when(instructionLoader.io.loadDone) {
+        // Ready to load data
+        when(memUsed === 0.U) { // Instruction memory
+          instructionMem.write(pc, instructionLoader.io.transferData)
+          pc := pc + 1.U
+          loadDoneReg := instructionMem.read(pc) // For LED indication
+        }
+        .otherwise { // Data memory
+          when(instructionLoader.io.loadDone) {
+            dataMem.write(pc, instructionLoader.io.transferData)
+            loadDoneReg := dataMem.read(pc) // For LED indication
+            pc := pc + 1.U
+          }
+        }
+        when(pc === length - 1.U) {
+          state := sIdle
+        }
+      }
+    }
   }
+  io.led := loadDoneReg
+
 }
 object UARTTop extends App {
     emitVerilog(new UARTTop(), Array("--target-dir", "generated"))
--- src/main/scala/UART/uploader.py
+++ src/main/scala/UART/uploader.py
@@ -1,24 +1,47 @@
 import serial
 import time
-import sys
+import struct
+from elftools.elf.elffile import ELFFile
 
 PORT = '/dev/ttyUSB1'  # Change this to your UART port
 BAUDRATE = 115200 # Shloud be the same for all of us, so dont change it
-FILE = "/home/ap/Dokumenter/RISCV-32_microprocesser/tests/ripes/add.bin"  # Change this to the binary file path you want to upload
+# Use elf file for meta data
+FILE = "/home/ap/Dokumenter/RISCV-32_microprocesser/tests/ripes/add.out"  # Change this to the elf file path you want to upload
 
 def upload_firmware(port, baudrate, file_path):
     ser = serial.Serial(port, baudrate, timeout=1)
     print(f"Opened port {port} at {baudrate} baud.")
     with open(file_path, 'rb') as f:
-        byte = f.read(1)
-
-        while byte:
-            ser.write(byte)
-            time.sleep(0.01)  # Small delay to ensure data is sent properly
-            byte = f.read(1)
+        elf = ELFFile(f)
+        text_section = elf.get_section_by_name('.text')
+        if text_section:
+            print(f"fount .text section")
+            text_data = text_section.data()
+            send_packet(ser, packet_type=0x00, data=text_data)
+        else:
+            print(f".text section not found in ELF file.")
+        
+        data_section = elf.get_section_by_name('.data')
+        if data_section:
+            print(f"fount .data section")
+            data_data = data_section.data()
+            send_packet(ser, packet_type=0x01, data=data_data)
+        else:
+            print(f".data section not found in ELF file.")
     
 
     print("Upload complete.")
     ser.close()
+
+def send_packet(ser, packet_type, data):
+    length = len(data)
+    header = struct.pack("<BI", packet_type, length) # < for little-endian, B for unsigned char, I for unsigned int (4 bytes)
+
+    print(f"sending header: type={packet_type} with length {length}")
+    ser.write(header)
+
+    ser.write(data)
+    time.sleep(0.01)  # Small delay to ensure data is sent properly
+
 
 upload_firmware(PORT, BAUDRATE, FILE)
