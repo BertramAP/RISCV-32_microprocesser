d573eacebc1ec1d2ead8f0b0d6e5aa5690ef9b32
AsgerJ0601 <s240601@dtu.dk>
2026-01-22 13:10:22 +0100

    Update .gitignor for CI

--- src/test/scala/PrimeTest.scala
+++ src/test/scala/PrimeTest.scala
@@ -11,25 +11,22 @@ class PrimeTest extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Prime Benchmark"
 
   it should "run prime.bin until completion" in {
-    val elfFile = "tests/Prime_benchmark/prime.elf"
-    if (!new java.io.File(elfFile).exists) {
-        cancel(s"Binary file $elfFile not found")
+    val binFile = "tests/Prime_benchmark/prime.bin"
+    if (!new java.io.File(binFile).exists) {
+        cancel(s"Binary file $binFile not found")
     }
-    val (imem, dmem, start) = ElfLoader.load(elfFile)
+    val (imem, dmem, start) = ElfLoader.load(binFile)
 
-    // Using 16KB memory size as used in InstructionTest (4096 words)
     val dmemSize = 4096 
     val newDmem = new Array[Int](dmemSize)
     val copyLen = math.min(dmem.length, dmemSize)
     Array.copy(dmem, 0, newDmem, 0, copyLen)
 
     test(new BenteTop(imem, newDmem, start, dmemSize)) { c =>
-      // Explicitly initialize inputs
       c.io.run.poke(false.B)
       c.io.imemWe.poke(false.B)
       c.io.dmemWe.poke(false.B)
 
-      // Initialize IMEM
       c.io.imemWe.poke(true.B)
       for (i <- imem.indices) {
          if (imem(i) != 0) {
@@ -60,6 +57,55 @@ class PrimeTest extends AnyFlatSpec with ChiselScalatestTester {
          cycles += 1
       }
       println(s"Prime Benchmark took $cycles cycles")
+    }
+  }
+  it should "run gcd.bin until completion" in {
+    val binFile = "tests/GCD_Benchmark/gcd.bin"
+    if (!new java.io.File(binFile).exists) {
+        cancel(s"Binary file $binFile not found")
+    }
+    val (imem, dmem, start) = ElfLoader.load(binFile)
+
+    val dmemSize = 4096 
+    val newDmem = new Array[Int](dmemSize)
+    val copyLen = math.min(dmem.length, dmemSize)
+    Array.copy(dmem, 0, newDmem, 0, copyLen)
+
+    test(new BenteTop(imem, newDmem, start, dmemSize)) { c =>
+      c.io.run.poke(false.B)
+      c.io.imemWe.poke(false.B)
+      c.io.dmemWe.poke(false.B)
+
+      c.io.imemWe.poke(true.B)
+      for (i <- imem.indices) {
+         if (imem(i) != 0) {
+             c.io.imemWaddr.poke(i.U)
+             c.io.imemWdata.poke((imem(i).toLong & 0xFFFFFFFFL).U)
+             c.clock.step(1)
+         }
+      }
+      c.io.imemWe.poke(false.B)
+
+      // Initialize DMEM
+      c.io.dmemWe.poke(true.B)
+      for (i <- 0 until copyLen) {
+           c.io.dmemWaddr.poke(i.U)
+           c.io.dmemWdata.poke((newDmem(i).toLong & 0xFFFFFFFFL).U)
+           c.clock.step(1)
+      }
+      c.io.dmemWe.poke(false.B)
+
+      // Run Simulation
+      c.io.run.poke(true.B)
+      
+      c.clock.setTimeout(50000000) 
+      
+      var cycles = 0
+      while (!c.io.done.peek().litToBoolean) {
+         c.clock.step(1)
+         cycles += 1
+      }
+      println(s"GCD Benchmark took $cycles cycles")
     }
   }
 }
